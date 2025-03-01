a55199d3fa723857d94f721a2f71291750f1781a
ashkatsu <inocencio.deanahashley@ue.edu.ph>
2025-03-01 14:28:31 +0800

    Create activity_2.cpp

---
+++ activity_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+int a[10];
+int ctr;
+for(ctr=0;ctr<=9;ctr++)
+{
+    cout<<"Enter any Number= ";
+    cin>>a[ctr];
+}
+cout<<“OUTPUT IN REVERSE ORDER”<<endl;
+for(ctr=9;ctr>=0;ctr--)
+    cout<<a[ctr]<<endl;
+return 0;
+}
